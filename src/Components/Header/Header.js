70969c1e93189b361651f69c81e289899a485a4f
Helcio <helcioanicio19@gmail.com>
2023-03-07 09:58:36 -0300

    Created the component Header and Header.modules.scss to add on App.js

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.modules.scss'
+
+function Header() {
+    return (
+      <>
+        <div className='title'>
+          <h1>Learn to code by watching others</h1>
+          <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+        </div>
+        
+      </>
+    );
+  }
+  
+  export default Header;
