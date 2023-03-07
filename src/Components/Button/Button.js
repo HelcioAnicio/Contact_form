e1415607c199648c49265d37aec07bd18046b416
Helcio <helcioanicio19@gmail.com>
2023-03-07 09:57:56 -0300

    Created the component Button and Button.modules.scss to add on App.js

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,16 @@
+import './Button.modules.scss'
+
+function Button() {
+    return (
+        <>
+            <div className='DivButton'>
+                <button>
+                    Try it free 7 days <span>then<br/>
+                    $20/mo.thereafter</span>
+                </button>
+            </div>        
+        </>
+    )
+}
+
+export default Button;
