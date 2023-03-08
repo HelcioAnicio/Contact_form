99a1c0bb8e790375eb243230b8d179a9f6fe6fba
Helcio <helcioanicio19@gmail.com>
2023-03-08 00:03:42 -0300

    Configured the component Header and Styled

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.modules.scss'
 function Header() {
     return (
       <>
-        <div className='title'>
+        <div className='DivTitle'>
           <h1>Learn to code by watching others</h1>
           <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
         </div>
