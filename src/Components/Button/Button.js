e18eb9d7767ff1b5cba2e69d4c0b6df756ec4a59
Helcio <helcioanicio19@gmail.com>
2023-03-08 00:03:22 -0300

    Configured the component Button and Styled

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -5,8 +5,7 @@ function Button() {
         <>
             <div className='DivButton'>
                 <button>
-                    Try it free 7 days <span>then<br/>
-                    $20/mo.thereafter</span>
+                    Try it free 7 days <span>then $20/mo.thereafter</span>
                 </button>
             </div>        
         </>
