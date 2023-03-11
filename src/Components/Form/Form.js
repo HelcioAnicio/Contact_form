0fd33723b93df8215d38eb02498152f78d90c11c
Helcio <helcioanicio19@gmail.com>
2023-03-10 23:33:54 -0300

    Improve the form logic and add more content in form.js

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,20 +1,88 @@
 import './Form.modules.scss'
-
+import { useState } from 'react';
 
 function Form() {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [formValid, setFormValid] = useState(false)
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+    }
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const cadastrarUsuario = (e) => {
+        e.preventDefault()
+
+        // Verifica se todos os campos foram preenchidos corretamente
+        if (firstName && lastName && email && password.length >= 8) {
+            console.log(`O Usuário foi cadastrado com os seguintes dados:
+                First Name:${firstName};
+                Last Name:${lastName};
+                Email:${email};
+                Password:${password}
+            `)
+            setFormValid(true)
+        } else {
+            setFormSubmitted(true)
+            setFormValid(false)
+        }
+    }
 
     return (
         <>
             <div className="DivForm">
-                <form action="">
+                <form onSubmit={cadastrarUsuario}>
+                    <input
+                        type="text"
+                        name="firstName"
+                        placeholder="First Name"
+                        onChange={handleFirstName}
+                        value={firstName}
+                    />
+                    {formSubmitted && firstName === '' && <input className= {styles.messageError} />}
+                    {formSubmitted && firstName === '' && <div className="Error"><p>Please fill in the first name correctly.</p></div>}
 
-                    <input required type="text" value="" name="FirstName"  placeholder="First Name" />
-                    
-                    <input required type="text" value="" name="LastName" placeholder="Last Name" />
-                    
-                    <input required type="email" value="" name="Email" placeholder="Email Address" />
-                    
-                    <input required type="password" value="" name="Password" placeholder="Password" />
+                    <input
+                        type="text"
+                        name="lastName"
+                        placeholder="Last Name"
+                        onChange={handleLastName}
+                        value={lastName}
+                    />
+                    {formValid === false && lastName === '' ? <div className="Error"><p>Please fill in the last name correctly.</p></div> : null}
+
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                        onChange={handleEmail}
+                        value={email}
+                    />
+                    {formValid === false && email === '' ? <div className="Error"><p>Please fill in the email address correctly.</p></div> : null}
+
+                    <input
+                        type="password"
+                        name="password"
+                        minLength={8}
+                        placeholder="Password"
+                        onChange={handlePassword}
+                        value={password}
+                    />
+                    {formValid === false && password.length < 8 ? <div className="Error"><p>Please fill in the password correctly. It should have at least 8 characters.</p></div> : null}
 
                     <input type="submit" value="CLAIM YOUR FREE TRIAL" />
 
@@ -23,7 +91,7 @@ function Form() {
                 <p>By clicking the button, you are agreeing to our <span>Terms and Services</span></p>
 
             </div>
-        
+
         </>
     );
 }
